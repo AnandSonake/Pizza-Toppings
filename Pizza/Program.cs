64e4c2859519a2dec6adc6038f9708596dbddc0a
AnandSonake <anandsonake@gmail.com>
2016-05-14 00:11:01 +0530

    initial commit
    top 20 pizza toppings

---
+++ Pizza/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace Pizza
+{
+    class Pizza
+    {
+        public string [] Toppings { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("pizzas.json");
+            var parsed = JsonConvert.DeserializeObject<Pizza[]>(data);
+            var result = parsed
+                       .Select(x => string.Join(",", x.Toppings.OrderBy(y => y)))
+                       .GroupBy(x => x)
+                       .OrderByDescending(x => x.Count())
+                       .Take(20)
+                       .ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine(string.Join(",", item.Key));
+            }          
+        }
+    }
+}
